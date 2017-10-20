29e47ef9c9cb1eaca793bddc821b7929adf5d53d
Michèl Borgman <mpborgman@hotmail.com>
2017-10-20 16:59:10 +0200

    Update on settings + Urls for REST

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+# firstName,addressStreet,lastName,addressCountry,
+# emailAddress,addressHouseNumber,password,addressCity
+
 # Create your models here.
+class Consumer(models.Model):
+    ID = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address = models.CharField(max_length=254)
+    password = models.CharField(max_length=30)
+
+class address(models.Model):
+    consumer = models.ForeignKey(Consumer, on_delete=models.CASCADE)
+    address_street = models.CharField(max_length=43)
+    address_country = models.CharField(max_length=40)
+    address_house_number = models.SmallIntegerField()
+    address_city = models.CharField(max_length=24)
