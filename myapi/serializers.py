06fb43fb3141deb48d4bf374e6740587f05974d4
Michèl Borgman <mpborgman@hotmail.com>
2017-10-24 15:51:06 +0200

    Address as nested object
    Return Address as nested

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from myapi.models import Consumer, Address
 
-class ConsumerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Consumer
-        fields = ('id', 'first_name', 'last_name', 'email_address', 'password', 'address')
-
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = ('id', 'address_street', 'address_country', 'address_house_number', 'address_city')
+
+class ConsumerSerializer(serializers.ModelSerializer):
+    address = AddressSerializer(many=False, read_only=True)
+    
+    class Meta:
+        model = Consumer
+        fields = ('id', 'first_name', 'last_name', 'email_address', 'password', 'address')
